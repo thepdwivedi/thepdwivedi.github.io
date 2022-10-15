621c8230c607f4559df90c32f6d75a1a47e28fdb
Pankaj Dwivedi <developer.dbrothers@gmail.com>
2022-10-14 23:08:58 -0400

    home added

---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './home.module.scss';
+
+const Home = () => (
+  <div className={styles.Home}>
+    Home Component
+  </div>
+);
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
