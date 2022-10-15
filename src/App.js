67fc54cfec9dea91f7545ea15a34f42619d7ae70
Pankaj Dwivedi <developer.dbrothers@gmail.com>
2022-10-15 10:08:57 -0400

    chrono added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import Home from "./components/home/home";
 import "./App.css";
-
+// importing aos
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-sm">One of three columns</div>
-              <div class="col-sm">One of three columns</div>
-              <div class="col-sm">One of three columns</div>
-            </div>
-          </div>
-        </a>
-      </header>
+      <Home></Home>
     </div>
   );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,627 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './home.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./home.module.scss";
+import bg from "../../public/images/bg.jpeg";
+import { FaBeer } from "react-icons/fa";
 
 const Home = () => (
   <div className={styles.Home}>
-    Home Component
+    <div className="container">
+      <div className={styles.Tagline}>
+        Welcome to my Profile, <br />
+        Pankaj Dwivedi
+      </div>
+      <div
+        className={styles.Profile}
+        data-aos="zoom-out-right"
+        data-aos-anchor-placement="top-center"
+      >
+        <img
+          src={bg}
+          alt="..."
+          width="200"
+          height="200"
+          class="rounded-circle"
+        ></img>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div data-aos="zoom-out-right" data-aos-anchor-placement="top-center">
+        <img
+          src={bg}
+          alt="..."
+          width="200"
+          height="200"
+          class="rounded-circle"
+        ></img>
+      </div>
+
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+    </div>{" "}
+    <div class="container">
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+    </div>{" "}
+    <div class="container">
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+    </div>{" "}
+    <div class="container">
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-primary "
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-success"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+      <div className="row h-50">
+        <div
+          className="col alert alert-danger"
+          role="alert"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          1 of 2
+        </div>
+        <div
+          className="col alert alert-warning"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+        >
+          2 of 2
+        </div>
+      </div>
+    </div>
   </div>
 );
 
